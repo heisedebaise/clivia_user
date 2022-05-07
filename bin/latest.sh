d30d459f62273d20dfb4144bf1d7c468eaa98818
lpw <lpw@clivia>
2022-05-07 09:38:34 +0800

    dev

--- bin/latest.sh
+++ bin/latest.sh
@@ -9,12 +9,8 @@ for i in `seq 0 $[${#dependencies[*]}-1]`; do
   flutter pub remove ${dependencies[i]}
 done
 
-flutter pub remove clivia_base
-
 for i in `seq 0 $[${#dependencies[*]}-1]`; do
   flutter pub add ${dependencies[i]}
 done
-
-flutter pub add clivia_base --path=../clivia_base
 
 flutter pub get
